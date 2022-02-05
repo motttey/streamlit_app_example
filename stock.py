4fd18337cd9ef2d11901c56e573a2d91e9bf38cd
motttey <motitago@gmail.com>
2022-02-05 15:11:26 +0900

    visualize plot as linechart

--- stock.py
+++ stock.py
@@ -16,14 +16,22 @@ url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
 def load_data():
     sp500_list = pd.read_html(url)[0].Symbol.values.tolist()
     data = yf.download(random.sample(sp500_list, 10), '2022-01-01','2022-02-01')
-    return data
+    return data['Close']
+
+def lineplot(df):
+    fig = plt.figure(figsize=(10,4))
+    sns.lineplot(data=df, x='Date', y='Close', hue='ticker')
+    st.pyplot(fig)
 
 sp500_list = load_data()
 
 st.title('S&P 500')
 
-st.markdown('''
-length ({})
-'''.format(len(sp500_list)))
+st.dataframe(sp500_list)
+stacked_df = sp500_list.stack().reset_index()
+stacked_df = stacked_df.rename(columns={
+    'level_1': 'ticker',
+    0: 'Close'
+})
 
-st.dataframe(sp500_list.head())
+lineplot(stacked_df)
