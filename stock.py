a720cf0655cf0b250fae57224e5ff2125a782a1f
motttey <motitago@gmail.com>
2022-02-06 10:04:34 +0900

    show tickers using small multiples

--- stock.py
+++ stock.py
@@ -23,6 +23,32 @@ def lineplot(df):
     sns.lineplot(data=df, x='Date', y='Close', hue='ticker')
     st.pyplot(fig)
 
+# https://seaborn.pydata.org/examples/timeseries_facets.html
+def smallMultiples(df):
+    g = sns.relplot(
+        data=df,
+        x="Date", y="Close", col="ticker", hue="ticker",
+        kind="line", palette="crest", linewidth=2.5, zorder=5,
+        col_wrap=2, height=2, aspect=1.5, legend=False,
+    )
+
+    g.tight_layout()
+    for year, ax in g.axes_dict.items():
+
+        # Add the title as an annotation within the plot
+        ax.text(.8, .85, year, transform=ax.transAxes, fontweight="bold")
+
+        # Plot every year's time series in the background
+        sns.lineplot(
+            data=df, x="Date", y="Close", units="ticker",
+            estimator=None, color=".7", linewidth=1, ax=ax,
+        )
+
+    # Reduce the frequency of the x axis ticks
+    ax.set_xticks(ax.get_xticks()[::4])
+
+    st.pyplot(g)
+
 sp500_list = load_data()
 
 st.title('S&P 500')
@@ -35,3 +61,5 @@ stacked_df = stacked_df.rename(columns={
 })
 
 lineplot(stacked_df)
+
+smallMultiples(stacked_df)
